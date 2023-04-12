ca50966350afe01fcde1b732c5f76d07651c86c5
aa-ps <aaronpuli45@gmail.com>
2023-04-12 18:08:35 -0500

    finish examples

--- Example 3/example3.js
+++ Example 3/example3.js
@@ -1,19 +1,36 @@
 // Import package
 const express = require("express");
+const sqlite3 = require('sqlite3');
+const { open } = require("sqlite");
 
 // Setup app to use ejs
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-// Hardcoded database
-const studentList=[{name:"Cassandra",major:"Computer Science"},{name:"Juan",major:"Biology"},{name:"Miguel",major:"Civil Engineering"},{name:"Andrew",major:"Computer Science"},{name:"Peter",major:"Mechanical Engineering"},{name:"Sophia",major:"Biology"},{name:"Briana",major:"English"}];
+// Connect to database
+let db;
+
+(async () => {
+  db = await open({
+    filename: "studentList.db",
+    driver: sqlite3.Database,
+  });
+})();
 
 // Render homepage
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const studentList = await db.all("SELECT * FROM students");
   res.render("index", {
     students: studentList,
   });
 });
+
+app.post("/addStudent", async (req, res) => {
+  const {name, major} = req.body;
+  await db.run("INSERT INTO students (name, major) VALUES (?,?)", [name, major]);
+  res.redirect("/");
+})
 
 // Start server
 app.listen(3000, () => {
