b57e7dec589162a7837f81fd2a7f3b7e0ff2e3c6
aa-ps <aaronpuli45@gmail.com>
2023-04-12 17:43:23 -0500

    changes

--- Example 2/example2.js
+++ Example 2/example2.js
@@ -1,14 +1,17 @@
+// Initialize variables
 const calculatorScreen = document.querySelector(".calculator-screen");
 const numbers = ["0","1","2","3","4","5","6","7","8","9"];
 const operators = ["*","/","+","-"];
-
 let problem = ""
 
 
+// Clears the calculator and problem to start fresh
 function resetCalculator() {
   calculatorScreen.value = "";
   problem = "";
 }
+
+// Function called from event listener. Handles button click
 
 function handleInput(event) {
     const value = event.target.value;
@@ -27,9 +30,11 @@ function handleInput(event) {
 
 }
 
+// Displays problem/result on the calculator screen
 function displayScreen(val){
     calculatorScreen.value = val; 
 }
 
+// Start the event listener for button clicks
 const keys = document.querySelector(".calculator-keys");
 keys.addEventListener("click", handleInput);
--- Example 3/example3.js
+++ Example 3/example3.js
@@ -1,7 +1,21 @@
-// JavaScript example using npm packages
-const express = require('express');
+// Import package
+const express = require("express");
+
+// Setup app to use ejs
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.set("view engine", "ejs");
+
+// Hardcoded database
+const studentList=[{name:"Cassandra",major:"Computer Science"},{name:"Juan",major:"Biology"},{name:"Miguel",major:"Civil Engineering"},{name:"Andrew",major:"Computer Science"},{name:"Peter",major:"Mechanical Engineering"},{name:"Sophia",major:"Biology"},{name:"Briana",major:"English"}];
+
+// Render homepage
+app.get("/", (req, res) => {
+  res.render("index", {
+    students: studentList,
+  });
 });
-app.listen(3000);
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
